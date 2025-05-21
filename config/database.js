fee1852b32aa01eaf12b17fe8143202cc66ce47c
Duvan Mina <duvan.andres312@gmail.com>
2025-05-21 12:58:11 -0500

    Enhance database configuration to support SSL connections

--- config/database.js
+++ config/database.js
@@ -30,6 +30,11 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
+        ssl: { 
+          require: false,
+          rejectUnauthorized: false,
+          ca: path.join(__dirname, 'ca.pem')
+        },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
