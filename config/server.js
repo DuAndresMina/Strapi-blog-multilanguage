5de0d8c2c33d86a871d1a3a1c341029138396cbd
Duvan Mina <duvan.andres312@gmail.com>
2025-05-21 13:27:06 -0500

    Server update

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: process.env.HOST || '0.0.0.0',
-  port: process.env.PORT || 4000,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 4000),
   app: {
     keys: env.array('APP_KEYS'),
   },
