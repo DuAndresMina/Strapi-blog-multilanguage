80ef97ea4051b35c53e2bd737593e726ed976d51
Duvan Mina <duvan.andres312@gmail.com>
2025-05-26 12:21:55 -0500

    Middlewares update supabase

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,31 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', process.env.SUPABASE_URL],
+          'default-src': ["'self'"],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            process.env.SUPABASE_URL,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            process.env.SUPABASE_URL,
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
